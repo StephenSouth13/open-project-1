acf713ca4760e09f42ac4cb840df46f567def71e
Quách Thành Long <stephensouth1307@gmail.com>
2026-06-06 05:06:31 +0700

    Update StopAgentSO.cs

--- UOP1_Project/Assets/Scripts/Characters/StateMachine/Actions/StopAgentSO.cs
+++ UOP1_Project/Assets/Scripts/Characters/StateMachine/Actions/StopAgentSO.cs
@@ -6,30 +6,31 @@ using UOP1.StateMachine.ScriptableObjects;
 [CreateAssetMenu(fileName = "StopAgent", menuName = "State Machines/Actions/Stop NavMesh Agent")]
 public class StopAgentSO : StateActionSO
 {
-	protected override StateAction CreateAction() => new StopAgent();
+    protected override StateAction CreateAction() => new StopAgent();
 }
 
 public class StopAgent : StateAction
 {
-	private NavMeshAgent _agent;
-	private bool _agentDefined;
+    private NavMeshAgent _agent;
+    private bool _agentDefined;
 
-	public override void Awake(StateMachine stateMachine)
-	{
-		_agent = stateMachine.gameObject.GetComponent<NavMeshAgent>();
-		_agentDefined = _agent != null;
-	}
+    public override void Awake(StateMachine stateMachine)
+    {
+        _agent = stateMachine.gameObject.GetComponent<NavMeshAgent>();
+        _agentDefined = _agent != null;
+    }
 
-	public override void OnUpdate()
-	{
+    public override void OnUpdate()
+    {
+        // Để trống
+    }
 
-	}
-
-	public override void OnStateEnter()
-	{
-		if (_agentDefined)
-		{
-			_agent.isStopped = true;
-		}
-	}
+    public override void OnStateEnter()
+    {
+        // KHIÊN BẢO VỆ CHỐNG CRASH Ở ĐÂY
+        if (_agentDefined && _agent.isActiveAndEnabled && _agent.isOnNavMesh)
+        {
+            _agent.isStopped = true;
+        }
+    }
 }
